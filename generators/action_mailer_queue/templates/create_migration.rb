ee046a0e9515a3f11d180d348610a08143a710e0
Andrew Beam <beam@server.int>
2008-10-08 00:11:51 +0200

    upravena migrace

--- generators/action_mailer_queue/templates/create_migration.rb
+++ generators/action_mailer_queue/templates/create_migration.rb
@@ -7,10 +7,10 @@ class Create<%= class_name %> < ActiveRecord::Migration
       t.column :subject,                  :string, :limit => 64, :default => nil
       t.column :content,                  :longblob
       t.column :message_id,               :string, :limit => 64
-      t.column :sent,                     :boolean, :default => false
-      t.column :attempts,                 :integer, :default => 0
+      t.column :sent,                     :boolean, :default => false, :null => false
+      t.column :attempts,                 :integer, :default => 0, :null => false
       t.column :last_error,               :string, :limit => 128, :default => nil
-      t.column :priority,                 :integer, :default => 10
+      t.column :priority,                 :integer, :default => 10, :null => false
       t.column :last_attempt_at,          :datetime, :default => nil
       t.column :sent_at,                  :datetime, :default => nil
       t.column :created_at,               :datetime, :default => nil
